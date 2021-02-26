c5f4e6bf365c3c660a594044293d852471088306
setiawananggi00 <anggi123setiawan@gmail.com>
2021-02-26 21:58:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function add() {
+    count += 1;
+    document.getElementById('counter').innerHTML = count;
+}
+
+function lower() {
+    count -= 1;
+    document.getElementById('counter').innerHTML = count;
+}
